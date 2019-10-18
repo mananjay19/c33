773ea66a1faad168f1f0be13cd2a989823d8c848
Apple <apple@arnies.local>
2019-10-18 18:37:14 +0530

    work on tragectory

--- sketch.js
+++ sketch.js
@@ -93,7 +93,9 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed<1){
+        bird.trajectory=[];
+        Matter.Body.setPosition(bird.body, {x: 200 , y: 50});
        slingshot.attach(bird.body);
     }
 }
